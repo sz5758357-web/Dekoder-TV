8b7cfca93824661f622e982f789a58e23d1195bb
sz5758357-web <sz5758357@gmail.com>
2026-07-29 22:02:50 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function loadChannel(name){
-    document.getElementById("title").textContent = name;
-
-    // Tutaj wstawiasz legalny adres transmisji,
-    // jeśli masz do niej uprawnienia.
-    document.getElementById("video").src = "";
-}
