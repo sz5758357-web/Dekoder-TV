1536058d56aec30c7aaf4cc9a36ec19fa7e376cf
sz5758357-web <sz5758357@gmail.com>
2026-08-02 22:24:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,108 @@
 const input = document.getElementById("m3uFile");
 const select = document.getElementById("channels");
 const player = document.getElementById("player");
+const nameBox = document.getElementById("channelName");
+const search = document.getElementById("search");
 
 let channels = [];
 
-input.addEventListener("change", e => {
-    const file = e.target.files[0];
 
-    const reader = new FileReader();
+input.addEventListener("change", e=>{
 
-    reader.onload = () => {
-        parseM3U(reader.result);
-    };
+const file=e.target.files[0];
 
-    reader.readAsText(file);
+const reader=new FileReader();
+
+reader.onload=()=>{
+parseM3U(reader.result);
+}
+
+reader.readAsText(file);
+
 });
+
 
 function parseM3U(text){
 
-    channels = [];
-    const lines = text.split("\n");
+channels=[];
 
-    for(let i=0;i<lines.length;i++){
+let lines=text.split("\n");
 
-        if(lines[i].startsWith("#EXTINF")){
-            const name = lines[i].split(",")[1].trim();
-            const url = lines[i+1].trim();
 
-            channels.push({name,url});
-        }
-    }
+for(let i=0;i<lines.length;i++){
 
-    select.innerHTML="";
+if(lines[i].startsWith("#EXTINF")){
 
-    channels.forEach((c,index)=>{
-        const option=document.createElement("option");
-        option.value=index;
-        option.textContent=c.name;
-        select.appendChild(option);
-    });
+let name=lines[i].split(",")[1].trim();
+let url=lines[i+1].trim();
 
-    if(channels.length){
-        player.src=channels[0].url;
-    }
+channels.push({
+name:name,
+url:url
+});
+
 }
 
+}
+
+
+showChannels(channels);
+
+
+if(channels.length){
+playChannel(0);
+}
+
+}
+
+
+
+function showChannels(list){
+
+select.innerHTML="";
+
+list.forEach((c,index)=>{
+
+let option=document.createElement("option");
+
+option.value=index;
+option.textContent=c.name;
+
+select.appendChild(option);
+
+});
+
+}
+
+
+
+function playChannel(i){
+
+let c=channels[i];
+
+player.src=c.url;
+nameBox.textContent=c.name;
+
+}
+
+
+
 select.addEventListener("change",()=>{
-    player.src=channels[select.value].url;
+
+playChannel(select.value);
+
+});
+
+
+
+search.addEventListener("input",()=>{
+
+let value=search.value.toLowerCase();
+
+let filtered=channels.filter(c=>
+c.name.toLowerCase().includes(value)
+);
+
+showChannels(filtered);
+
 });
